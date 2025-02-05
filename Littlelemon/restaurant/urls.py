3763a9f7d39b935c5ea32259a5bf10c7a192c8c6
darkmedo20 <ahmedmostafaomer@gmail.com>
2025-02-05 21:07:39 +0300

    Token Authentication

--- Littlelemon/restaurant/urls.py
+++ Littlelemon/restaurant/urls.py
@@ -1,9 +1,10 @@
 
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
     path('', views.MenuItemsView.as_view()),
     path('<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- Littlelemon/restaurant/views.py
+++ Littlelemon/restaurant/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -15,4 +16,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = BookingSerializer
+    
