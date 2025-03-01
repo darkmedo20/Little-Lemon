714f24e82aedf8f184514dc6e1e38b5dbafc84d2
darkmedo20 <ahmedmostafaomer@gmail.com>
2025-03-01 16:38:53 +0300

    static files changes

--- Littlelemon/Littlelemon/settings.py
+++ Littlelemon/Littlelemon/settings.py
@@ -80,8 +80,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Littlelemon.wsgi.application'
+import os
 
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = 'restaurant/static/'
+
+STATICFILES_DIRS = [
+    "restaurant/static",
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -134,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
